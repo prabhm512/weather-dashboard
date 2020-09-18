2f2f96ebd8f1ef8b7274f7cf6f860cf8f3c2478d
Prabh Singh <prabhm512@gmail.com>
2020-09-18 15:56:48 +1000

    Bug fix: list items already rendered were rendering again

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
-var cityInput = $("#city-input");
-var cityList = $(".city-list");
+$(document).ready(function() {
+    var cityInput = $("#city-input");
+    var cityList = $(".city-list");
+    
+    var cities = [];
+    
+    // Retrieves cities already that have already been searched for
+    function getCityList() {
+    
+        var storedCities = JSON.parse(localStorage.getItem("cities"));
+    
+        if (storedCities !== null) {
+    
+            cities = storedCities;
+        }
 
-var cities = [];
-
-function getCityList() {
-
-    var storedCities = JSON.parse(localStorage.getItem("cities"));
-
-    if (storedCities !== null) {
-
-        cities = storedCities;
+        // Initial Render
+        for (i=cities.length-1; i>=0; i--) {
+            var li = $("<li>");
+            li.append(cities[i]);
+            cityList.append(li);
+        }
     }
-    renderCityList();
-}
+    
+    getCityList();
+    
+    // Renders searched cities
+    function renderCityList() {
+    
+        for (i=cities.length-1; i>=0; i--) {
 
-getCityList();
+            // Cities already rendered should not re-render
+            
+            if (cityList.children().length >= i) {
+                continue;
+            }
+            
+            else {
 
-function renderCityList() {
+                // Render of newly searched city
+                var li = $("<li>");
+                li.append(cities[i]);
+                cityList.append(li);
+    
+            }
 
-    for (i=cities.length-1; i>=0; i--) {
-
-        var li = $("<li>");
-        li.append(cities[i]);
-        cityList.append(li);
+        }
     }
-}
-
-function setCityList() {
-
-    cities.push(cityInput.val());
-
-    localStorage.setItem("cities", JSON.stringify(cities));
-
-    renderCityList();
-}
-
-$(".submit-btn").on("click", setCityList);
+    
+    function setCityList() {
+        
+        // Push searched city to array
+        cities.push(cityInput.val());
+        // Update local storage
+        localStorage.setItem("cities", JSON.stringify(cities));
+    
+        renderCityList();
+    }
+    
+    $(".submit-btn").on("click", setCityList);
+})
